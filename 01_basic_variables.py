0cb5a95668ec4841ab90691e689de3321fb0d52e
Mariappan Sakthimurugan <s.mari352@gmail.com>
2026-08-08 16:25:38 +0530

    Add basic variables and print statements

---
+++ 01_basic_variables.py
@@ -0,0 +1,11 @@
+# Basic variables
+
+name = "Mariappan"
+age = 30
+salary = 50000
+is_working = True
+
+print("Name:", name)
+print("Age:", age)
+print("Salary:", salary)
+print("Working:", is_working)
