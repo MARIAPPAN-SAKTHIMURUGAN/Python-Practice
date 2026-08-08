3142cab03c059ec9fee8d03866a48df340ecc218
Mariappan Sakthimurugan <s.mari352@gmail.com>
2026-08-08 16:56:39 +0530

    Create 18_try_except_else_finally.py

---
+++ 18_try_except_else_finally.py
@@ -0,0 +1,15 @@
+try:
+    number = int(input("Enter a number: "))
+    result = 10 / number
+
+except ValueError as error:
+    print("Error:", error)
+
+except ZeroDivisionError as error:
+    print("Error:", error)
+
+else:
+    print("Result:", result)
+
+finally:
+    print("Calculation completed")
