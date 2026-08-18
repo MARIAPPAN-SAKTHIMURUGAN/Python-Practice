8ca229cf119b8df3343dc14d8de95aae474d9370
Mariappan Sakthimurugan <s.mari352@gmail.com>
2026-08-18 17:46:06 +0530

    Create 56_method_overriding.py

---
+++ 56_method_overriding.py
@@ -0,0 +1,46 @@
+class Fiber:
+    def __init__(self, fiber_id, length, status):
+        self.fiber_id = fiber_id
+        self.length = length
+        self.status = status
+        self.length_in_km = length / 1000
+
+    def display(self):
+        print("\n--- Fiber Details ---")
+        print("Fiber ID:", self.fiber_id)
+        print("Length:", self.length, "Meters")
+        print("Status:", self.status)
+        print("Length in KM:", self.length_in_km)
+
+
+class FiberOutput(Fiber):
+    def __init__(self, fiber_id, length, status, splitter_ratio):
+        super().__init__(fiber_id, length, status)
+        self.splitter_ratio = splitter_ratio
+
+
+class FTTHFiber(FiberOutput):
+
+    # Method Overriding
+    def display(self):
+        print("\n--- FTTH Fiber Details ---")
+        print("Fiber ID:", self.fiber_id)
+        print("Length:", self.length, "Meters")
+        print("Status:", self.status)
+        print("Length in KM:", self.length_in_km)
+        print("Splitter Ratio:", self.splitter_ratio)
+
+
+# Parent class object
+fiber1 = Fiber("E001", 450, "Active")
+
+# Child class object
+fiber2 = FTTHFiber("E002", 450, "Active", "1:8")
+
+# Parent class display()
+print("Parent Class:")
+fiber1.display()
+
+# Child class display() overrides parent display()
+print("\nChild Class (Method Overriding):")
+fiber2.display()
