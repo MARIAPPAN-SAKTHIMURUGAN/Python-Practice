2c9ad4b891f27c21d84505dddde086af19775398
Mariappan Sakthimurugan <s.mari352@gmail.com>
2026-08-23 12:09:59 +0530

    Add files via upload

---
+++ phase04_modules/03_custom_module/fiber_utility.py
@@ -0,0 +1,12 @@
+def calculate_length_inkm(length):
+    return length/1000
+def Vault_placement(length):
+    if length >1000:
+        return length/1000
+    else:
+        pass
+def splitter_calculation (homepass):
+    return homepass/2
+
+
+
