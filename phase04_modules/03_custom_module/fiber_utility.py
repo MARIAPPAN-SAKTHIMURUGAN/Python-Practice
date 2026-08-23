d2aa94608d3311415b158d39b75c8c2912d088a3
Mariappan Sakthimurugan <s.mari352@gmail.com>
2026-08-23 15:07:27 +0530

    Update fiber_utility.py

--- phase04_modules/03_custom_module/fiber_utility.py
+++ phase04_modules/03_custom_module/fiber_utility.py
@@ -1,12 +1,24 @@
 def calculate_length_inkm(length):
-    return length/1000
-def Vault_placement(length):
-    if length >1000:
-        return length/1000
-    else:
-        pass
-def splitter_calculation (homepass):
-    return homepass/2
+    return length / 1000
+
+
+def vault_placement(length):
+    return length / 1000
+
+
+def splitter_calculation(homepass):
+    return homepass / 32
+
+
+if __name__ == "__main__":
+    print("Testing Fiber Utility")
+
+    length = 4000
+    homepass = 700
+
+    print("Fiber Length:", calculate_length_inkm(length), "KM")
+    print("Vault Requirement:", vault_placement(length))
+    print("Splitter Requirement:", splitter_calculation(homepass))
 
 
 
